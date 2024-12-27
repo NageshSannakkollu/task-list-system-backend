982827a5b32b56753fffdd2d90313e5d8636e8fc
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2024-12-27 18:08:19 +0530

    latest dbconnection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
 const { default: mongoose } = require("mongoose")
 const express = require("express")
 require("dotenv").config()
-const dbUri = process.env.MONGO_URI
-console.log(dbUri)
 
 const DBConnection = async() => {
     try{
-        await mongoose.connect(dbUri,{
+        await mongoose.connect(`${process.env.MONGO_URI}/taskList`,{
             useNewUrlParser:true,
         })
         console.log("DB Connected Successfully")
